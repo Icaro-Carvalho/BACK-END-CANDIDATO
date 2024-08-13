2399cf49a50ac8ce9b36669d1b2102bba0bba3b7
Icaro Carvalho <icaropc16@gmail.com>
2024-08-13 14:09:56 -0300

    Estrtura do Spring

---
+++ src/main/java/br/com/ProjetoCandidato/ProjetoCandidatoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ProjetoCandidato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCandidatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCandidatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ProjetoCandidato/ProjetoCandidatoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ProjetoCandidato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCandidatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
